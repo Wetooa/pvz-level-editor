e2f02442b1328d9a172e07b77095afc2c23eb054
Wetooa <103910243+Wetooa@users.noreply.github.com>
2023-12-14 03:49:08 +0800

    added more stuff

--- src/PVZLevelEditor.java
+++ src/PVZLevelEditor.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class PVZLevelEditor extends  JFrame {
 
@@ -8,7 +9,6 @@ public class PVZLevelEditor extends  JFrame {
     private JPanel body;
     private JCheckBox cbPeashooter;
     private JButton saveChangesButton;
-    private JButton discardChangesButton;
     private JTabbedPane tabbedPane1;
     private JCheckBox cbNormalZombie;
     private JPanel cbPlants;
@@ -27,8 +27,8 @@ public class PVZLevelEditor extends  JFrame {
     private JCheckBox checkBox7;
     private JCheckBox checkBox8;
     private JCheckBox checkBox9;
-    private JList list1;
-    private JList list2;
+    private JList plantList;
+    private JList zombieList;
     private JCheckBox coneheadZombieCheckBox;
     private JCheckBox checkBox2;
     private JCheckBox checkBox10;
@@ -38,19 +38,61 @@ public class PVZLevelEditor extends  JFrame {
     private JSlider slider1;
     private JCheckBox cbSunflower;
     private JCheckBox checkBox1;
-    private JCheckBox checkBox3;
+    private JCheckBox chomperCheckBox;
+    private JCheckBox cbGatlingPea;
+    private JCheckBox cbRepeater;
     private JCheckBox checkBox6;
     private JCheckBox checkBox11;
+    private JCheckBox checkBox12;
+    private JCheckBox checkBox13;
+    private JCheckBox checkBox14;
+
+
+    public static final int MAX_PLANT_COUNT = 8;
+    public static final int MAX_ZOMBIE_COUNT = 12;
+
+    protected int plantCount = 0;
+    protected int zombieCount = 0;
+
+    protected ArrayList<String> selectedPlants;
+    protected ArrayList<String> selectedZombies;
+    protected int initialSun;
+    protected int sunValue;
+    protected int sunFallRate;
 
     public PVZLevelEditor() {
+        JOptionPane jOption = new JOptionPane();
+
         ActionListener cbPlantsActionListener = new ActionListener() {
+
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println(((JCheckBox)e.getSource()).getText());
+
+                JCheckBox cbPlant = ((JCheckBox)e.getSource());
+                String pName = cbPlant.getText();
+                boolean isSelected = cbPlant.isSelected();
+
+                if (isSelected) {
+
+                    if (plantCount == MAX_PLANT_COUNT) {
+                        jOption.createDialog("Maximum plant count! Remove one to add " + pName + " to the list");
+                        return;
+                    }
+
+                    ++plantCount;
+                    selectedPlants.add(pName);
+                } else {
+                    selectedPlants.remove(pName);
+                }
             }
         };
 
-        cbPeashooter.addActionListener(cbPlantsActionListener );
+        cbPeashooter.addActionListener(cbPlantsActionListener);
+        cbSunflower.addActionListener(cbPlantsActionListener);
+        cbRepeater.addActionListener(cbPlantsActionListener);
+        cbGatlingPea.addActionListener(cbPlantsActionListener);
+//        .addActionListener(cbPlantsActionListener);
+//        cbSunflower.addActionListener(cbPlantsActionListener);
 
         ActionListener cbZombiesActionListener = new ActionListener() {
             @Override
@@ -60,6 +102,19 @@ public class PVZLevelEditor extends  JFrame {
         };
 
         cbNormalZombie.addActionListener(cbPlantsActionListener);
+
+        saveChangesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        resetToDefaultButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 
     public static void main(String[] args) {
